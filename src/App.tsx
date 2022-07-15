b2b363f60b70588bbb31108de3387ec2063a03fd
Anubhab Karki <bhubhik@gmail.com>
2022-07-15 20:46:58 +1000

    Defining props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Myprops from './Myprops';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppProps {
+  color?: string;
 }
+const App = (props: AppProps): JSX.Element => {
+  const color = 'red';
+  return (
+    <>
+      <TryCatch color={color} />
+      <Myprops color={color} />
+    </>
+  );
+};
+
+const TryCatch = ({ color }: AppProps) => {
+  return <div>{color}</div>;
+};
 
 export default App;
---
+++ src/Myprops.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppProps } from './App';
+
+const Myprops = ({ color }: AppProps) => {
+  return <div>{color}</div>;
+};
+
+export default Myprops;
