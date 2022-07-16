894851fad39f8634e227875f1b8a6048daa89db0
Anubhab Karki <bhubhik@gmail.com>
2022-07-16 11:10:03 +1000

    Image component removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import React, { MouseEventHandler, useEffect, useState } from 'react';
-import { faker } from '@faker-js/faker';
-import Image from './components/Image';
 import User from './components/User';
 
-export interface AppProps {
-  img?: string;
-  nextImg?: MouseEventHandler<HTMLElement>;
-}
-
-const App = (props: AppProps): JSX.Element => {
-  const [img, setImg] = useState('');
-
-  const nextImg = () => {
-    setImg(faker.image.abstract(450, 500, true));
-    console.log(img);
-  };
-  useEffect(() => {
-    nextImg();
-  }, []);
+const App = (): JSX.Element => {
   return (
     <>
       <User />
-      <Image img={img} nextImg={nextImg} />
     </>
   );
 };
